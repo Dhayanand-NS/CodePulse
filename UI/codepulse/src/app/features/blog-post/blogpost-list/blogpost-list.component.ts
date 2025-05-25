aa3ab1e4dfd4afa616e9ecf1860ea0f5c8ab0380
Dhayanand-NS <dhayanandns70@gmail.com>
2025-05-25 11:49:48 +0530

    Section 8

--- UI/codepulse/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
+++ UI/codepulse/src/app/features/blog-post/blogpost-list/blogpost-list.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { BlogPostService } from '../services/blog-post.service';
+import { Observable } from 'rxjs';
+import { BlogPost } from '../models/blogPost.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-blogpost-list',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './blogpost-list.component.html',
   styleUrl: './blogpost-list.component.css'
 })
-export class BlogpostListComponent {
+export class BlogpostListComponent implements OnInit{
+blogPost$? :Observable<BlogPost[]>
+
+  constructor( private blogPostService : BlogPostService){
+
+  }
+  ngOnInit(): void {
+     this.blogPost$=this.blogPostService.getAllBlogPosts();
+  }
 
 }
--- UI/codepulse/src/app/features/blog-post/services/blog-post.service.ts
+++ UI/codepulse/src/app/features/blog-post/services/blog-post.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { AddBlogPost } from '../models/add-blogpost.model';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { BlogPost } from '../models/blogPost.model';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,12 @@ import { environment } from '../../../../environments/environment';
 export class BlogPostService {
 
   constructor(private http : HttpClient) {}
+
     addBlogPost (model : AddBlogPost): Observable<void>{
       return this.http.post<void>(`${environment.apiBaserl}api/BlogPost`, model)
+    }
+
+    getAllBlogPosts() : Observable<BlogPost[]>{
+      return this.http.get<BlogPost[]>(`${environment.apiBaserl}api/BlogPost`)
     }
 }
